0887c878cde2708d60f655f510cf7e60097a91c7
caioviana <caio.devwork@gmail.com>
2026-03-11 20:23:23 -0300

    Initial setup

---
+++ src/test/java/spring/salestracker/api/SalesTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.salestracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
